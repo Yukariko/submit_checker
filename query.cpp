5abf08f41ae85e088ba57dae49e80120c2950758
Yukariko <rkdals7979@naver.com>
2015-11-17 20:03:48 +0900

    bug fix

--- query.cpp
+++ query.cpp
@@ -3,7 +3,7 @@
 Query::Query(const MYSQL_ROW row)
 {
 	for(int i=0; row[i]; i++)
-		results.push_back(row[i]);
+		results.push_back(string(row[i], mysql_fetch_lengths(row[i])));
 }
 
 const string& Query::getResult(int idx) const
